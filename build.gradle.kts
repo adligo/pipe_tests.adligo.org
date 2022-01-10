58e0cd76d73bba555697d2a136c18060662e7cab
Scott Morgan <scott@adligo.com>
2022-01-09 22:37:13 -0600

    Project files for Eclpise Import.

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+/**
+  NOTE use the project git@github.com:adligo/2022.git to build this
+*/
+
+plugins {
+  eclipse
+  java
+}
+
+sourceSets {
+  main {
+    java {
+      srcDirs("src")
+    }
+  }
+}
+
+
+
+
